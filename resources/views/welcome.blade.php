b03014c8c01483fd7c6140349875927b32b872dc
restyindah <restyindah34@gmail.com>
2019-07-02 14:01:58 +0700

    Hosting pertama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Resty Indah
                 </div>
 
                 <div class="links">
